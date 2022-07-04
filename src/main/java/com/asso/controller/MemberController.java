dfadc8f6e46e4590f21c361332f2349e9590c92c
riki89 <saikoudiallo59@gmail.com>
2022-07-04 09:33:41 -0500

    Solving constraint issue

--- src/main/java/com/asso/controller/MemberController.java
+++ src/main/java/com/asso/controller/MemberController.java
@@ -5,9 +5,16 @@ import com.asso.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/member")
@@ -16,7 +23,21 @@ public class MemberController {
     private MemberService memberService;
 
     @PostMapping()
-    public ResponseEntity<?> add(@RequestBody Member member) {
+    public ResponseEntity<?> add(@RequestBody Member member, ConstraintViolationException exceptions) {
+        if (exceptions != null) {
+            Map<String, Object> fieldError = new HashMap<>();
+            Set<ConstraintViolation<?>> fieldErrors= exceptions.getConstraintViolations();
+            for (ConstraintViolation cv : fieldErrors) {
+                fieldError.put(cv.getPropertyPath().toString(), cv.getMessage());
+            }
+            Map<String, Object> map = new HashMap<>();
+            map.put("isSuccess", false);
+            map.put("data", null);
+            map.put("status", HttpStatus.BAD_REQUEST);
+            map.put("fieldError", fieldError);
+            //Map with all the errors
+            return new ResponseEntity<Object>(map,HttpStatus.BAD_REQUEST);
+        }
         Member member1 = memberService.add(member);
         if (member1 != null) {
             return new ResponseEntity<>(member1, HttpStatus.OK);
--- src/main/java/com/asso/model/Member.java
+++ src/main/java/com/asso/model/Member.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +21,12 @@ public class Member {
     @Id
     @GeneratedValue
     private Integer id;
+    @NotNull
     private String lastName;
+    @NotNull
     private String firstName;
+    @NotNull
+//    @Digits(integer = 9, fraction = 0, message = "Phone number should have exactly 10 characters")
     private String phoneNumber;
     private String sex;
     @JsonFormat(pattern = "dd/MM/yyyy")
